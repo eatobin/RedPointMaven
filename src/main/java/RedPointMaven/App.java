63ece2825ba0e8db005d60ce14b5b2328c245532
Eric Tobin <eatobin@gmail.com>
2012-08-23 12:13:10 -0500

    initial commit

---
+++ src/main/java/RedPointMaven/App.java
@@ -0,0 +1,13 @@
+package RedPointMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
