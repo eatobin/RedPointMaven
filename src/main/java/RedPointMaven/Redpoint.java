f4ec1193ceb29c8d0d3c6d986da26b2588b13c23
Eric Tobin <eatobin@gmail.com>
2013-04-21 21:54:00 -0500

    progress

--- src/main/java/RedPointMaven/Redpoint.java
+++ src/main/java/RedPointMaven/Redpoint.java
@@ -1,14 +1,11 @@
 package RedPointMaven;
 
-/**
- * Main class for RedpointMaven.
- */
-
 import java.util.Scanner;
 
 public class Redpoint {
     static Roster blackhawks2010;
     static Hat giveeHat;
+    static int doNextYear;
 
     private static String giveePass(String giver, String givee, int year) {
         blackhawks2010.setGiveeCode(giver, givee, year);
@@ -39,10 +36,10 @@ public class Redpoint {
         }
     }
 
-    private static int printGifts(int year) {
+    private static int printGiftsAndNext(int year) {
         Scanner scanner;
         scanner = new Scanner(System.in);
-        int doNextYear;
+
         System.out.println("Year " + year + " Gifts:");
         blackhawks2010.printGivingRoster(year);
         System.out.println();
@@ -52,57 +49,28 @@ public class Redpoint {
         return doNextYear;
     }
 
-    // main method
     public static void main(String[] args) {
         int year = 0;
-        int doNextYear;
         blackhawks2010 = new Roster();
         String giver;
         String givee;
         Hat giverHat;
 
-        doNextYear = printGifts(year);
-
-        while (doNextYear == 1) {
+        while (printGiftsAndNext(year) == 1) {
             year++;
-
-            // add a new, empty year to history
             blackhawks2010.addNewYear();
-
-            // make a giver Hat
             giverHat = new Hat(blackhawks2010);
-
-            // make a givee Hat
             giveeHat = new Hat(blackhawks2010);
-
-            // draw a giver
             giver = giverHat.drawPuck();
-
-            // draw a givee
             givee = giveeHat.drawPuck();
-
-            // keep drawing givers until Hat empty
             while (!giver.equals("none")) {
-                /*
-                keep drawing a givee for giver until all 3 tests are true.
-                discard givee if test fails.
-                set givee for giver when full success.
-                remove givee from Hat.
-                remover giver from Hat.
-                replace discard givees.
-                draw new giver and repeat all.
-                */
                 applyRules(year, giver, givee);
                 giverHat.removePuck(giver);
                 giveeHat.returnDiscards();
                 giver = giverHat.drawPuck();
                 givee = giveeHat.drawPuck();
             }
-            doNextYear = printGifts(year);
         }
-
-        // hope to hear from you!!
-        System.out.println();
         System.out.println("This was fun!");
         System.out.println("Talk about a position with Redpoint?");
         System.out.println("Please call: Eric Tobin 773-325-1516");
