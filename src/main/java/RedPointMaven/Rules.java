8107ea45165fad1fcd3047c9ee6d061e55b3c81e
Eric Tobin <eatobin@gmail.com>
2014-12-10 17:30:25 -0600

    fixed Rules - class visibility and constant for years back

--- src/main/java/RedPointMaven/Rules.java
+++ src/main/java/RedPointMaven/Rules.java
@@ -1,24 +1,25 @@
 package RedPointMaven;
 
-public class Rules {
+class Rules {
     //test 1 of 3 - is giver giving to self?
-    public static boolean giveeNotSelf(String giverCode, String giveeCode) {
+    static boolean giveeNotSelf(String giverCode, String giveeCode) {
         return !giverCode.equals(giveeCode);
     }
 
     //test 2 of 3 - is givee giving to giver?
-    public static boolean giveeNotRecip(String giverCode, String giveeCode, Roster roster, int thisYear) {
+    static boolean giveeNotRecip(String giverCode, String giveeCode, Roster roster, int thisYear) {
         String giveeGivingTo;
         giveeGivingTo = roster.getGiveeCode(giveeCode, thisYear);
         return !giverCode.equals(giveeGivingTo);
     }
 
-    //test 3 of 3 - has giver given to givee in past 4 years?
-    public static boolean giveeNotRepeat(String giverCode, String giveeCode, Roster roster, int thisYear) {
+    //test 3 of 3 - has giver given to givee in past X years?
+    static boolean giveeNotRepeat(String giverCode, String giveeCode, Roster roster, int thisYear) {
+        int PAST_X_YEARS = 4;
         int counter;
         String giveeInYear;
         boolean result = true;
-        for (counter = thisYear - 1; (counter >= 0) && (counter >= (thisYear - 4)); counter--) {
+        for (counter = thisYear - 1; (counter >= 0) && (counter >= (thisYear - PAST_X_YEARS)); counter--) {
             giveeInYear = roster.getGiveeCode(giverCode, counter);
             if (giveeCode.equals(giveeInYear)) {
                 result = false;
