0fac32adac4412a42564cff4fbc642ea3504f2f9
Eric Tobin <eatobin@gmail.com>
2014-12-16 12:51:23 -0600

    added fileName variable

--- src/main/java/RedPointMaven/Redpoint.java
+++ src/main/java/RedPointMaven/Redpoint.java
@@ -3,9 +3,9 @@ package RedPointMaven;
 import java.util.Scanner;
 
 class Redpoint {
-
+    private static final String fileName = "blackhawks2010.txt";
     private static int year = 0;
-    private static final Roster ROSTER = new Roster("blackhawks2010.txt");
+    private static final Roster ROSTER = new Roster(fileName);
     private static Hat giverHat;
     private static Hat giveeHat;
     private static String giver;
